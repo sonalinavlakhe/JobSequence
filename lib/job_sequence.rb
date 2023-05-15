e6f2414072060f97c57c7deda2c0b290f144be5e
Sonali Navlakhe <40335339+sonalinavlakhe@users.noreply.github.com>
2023-05-15 07:27:51 +0530

    Add method sequence_jobs for job sequence

--- lib/job_sequence.rb
+++ lib/job_sequence.rb
@@ -3,6 +3,30 @@ class JobSequence
     @jobs = jobs
   end
 
+  def sequence_jobs
+    validate_jobs
+    
+    job_queue = []
+    job_dependencies = @jobs.clone
+
+    job_dependencies.each do |job, dependency|
+      if dependency.nil?
+        job_queue << job
+        job_dependencies.delete(job)
+      end
+    end
+
+    while job_dependencies.any?
+      job_dependencies.each do |job, dependency|
+        next unless job_queue.include?(dependency)
+        job_queue << job
+        job_dependencies.delete(job)
+      end
+    end
+
+    job_queue.join('')
+  end
+
   def validate_jobs
      begin
       @jobs.each do |job, dependency|
@@ -16,11 +40,10 @@ class JobSequence
 
   private
 
-
   def circular_dependency?(job, dependency)
     return false if dependency.nil?
     visited_jobs = []
-    
+
     while @jobs[dependency]
       visited_jobs << dependency
       dependency = @jobs[dependency]
